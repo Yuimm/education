32036ddc39f2aa4a4931282f1fd4a4afadf393c7
yuimm <974288521@qq.com>
2021-04-25 13:01:11 +0800

    项目搭建

---
+++ service/service_edu/src/main/java/com/hct/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.hct.eduservice.mapper;
+
+import com.hct.eduservice.pojo.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author HCT
+ * @since 2021-04-25
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
